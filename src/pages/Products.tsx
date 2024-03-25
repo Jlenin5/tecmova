9ad520d140668e7a52141dedae758790098295cd
Jlenin5 <jlenin25mv@gmail.com>
2024-03-25 10:54:35 -0500

    filter_products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -12,17 +12,14 @@ import LoadProducts from './loading/LoadProducts'
 const Products = () => {
   const dispatch = useAppDispatch()
   const products = useAppSelector((state:any) => state.products)
-
   const [dCategory,setDCategory] = useState<Category[]>([])
-  const [data, setData] = useState(products)
   const [loading, setLoading] = useState(true)
   const [selectedCategory, setSelectedCategory] = useState<number>(0)
-  const [dataProduct,setDataProduct] = useState<Product[]>([])
+  const [checkedCategory, setCheckedCategory] = useState<Set<number>>(new Set())
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([])
 
   useEffect(() => {
     dispatch(getProducts(null)).then((response) => {
-      setDataProduct(response.payload)
       setFilteredProducts(response.payload)
       setLoading(false)
     })
@@ -31,27 +28,40 @@ const Products = () => {
 
   const clickedCategory = (category: number) => {
     if(category===0) {
-      setFilteredProducts(dataProduct)
+      setFilteredProducts(products)
       setSelectedCategory(0)
     } else {
       const filteredProductByCategory = products.filter((product: Product) =>
         product.categories.some((cat) => cat.id === category)
       )
-      console.log(filteredProductByCategory)
       setFilteredProducts(filteredProductByCategory)
       setSelectedCategory(category)
     }
   }
 
-  useEffect(() => {
-    // getCategories().then(r => setDataCategory(r))
-    // getProducts().then((r) => {
-    //   setDataProduct(r)
-    //   setFilteredProducts(r)
-    // })
-  }, [])
+  const checkedFilterCategory = (categories: number) => {
+    if(!categories) {
+      setCheckedCategory(new Set())
+      setFilteredProducts(products)
+    } else {
+      const updatedCategories = new Set(checkedCategory)
+      if (updatedCategories.has(categories)) {
+        updatedCategories.delete(categories)
+      } else {
+        updatedCategories.add(categories)
+      }
+      setCheckedCategory(updatedCategories)
+      let filteredProductByCategory = products
+      if (updatedCategories.size > 0) {
+        filteredProductByCategory = products.filter((product: Product) =>
+          product.categories.some((cat) => updatedCategories.has(cat.id))
+        )
+      }
+      setFilteredProducts(filteredProductByCategory)
+    }
+  }
 
-  if(data.length === 0) {
+  if(products.length === 0) {
     return (
       <h1>No hay datos</h1>
     )
@@ -76,30 +86,48 @@ const Products = () => {
                 <li key={r.id} onClick={() => clickedCategory(r.id)} className={selectedCategory === r.id ? 'categorySelected' : 'categoryNoSelected'}>{r.cateName}</li>
               ))}
             </ul>
-            <ul className="products">
-              {filteredProducts.map((r) => (
-                <li key={r.id}>
-                  <div className="box-img">
-                  {r.product_images.find((image) => image.featured === r.featuredImageId) && (
-                    <img
-                      src={`https://sismova.tech/backsis/public/images/products/${
-                        r.product_images.find((image) => image.featured === r.featuredImageId)?.primPath
-                      }`}
-                      alt={r.prodName}
-                    />
-                  )}
-                  </div>
-                  <div className="info-prod">
-                    <div className="card-product-description">
-                      <span>{r.prodName}</span>
+            <div className="ctn-filter-products">
+              <ul className="filters">
+                <span>Filtros</span>
+                <div className="hr"></div>
+                {
+                  dCategory.map(r => (
+                    <li key={r.id} onClick={() => checkedFilterCategory(r.id)}>
+                      <div className={`box-check ${checkedCategory.has(r.id) ? 'selected' : ''}`}>
+                        <i className="fa-solid fa-check"></i>
+                      </div>
+                      <div className="name">
+                        {r.cateName}
+                      </div>
+                    </li>
+                  ))
+                }
+              </ul>
+              <ul className="products">
+                {filteredProducts.map((r) => (
+                  <li key={r.id}>
+                    <div className="box-img">
+                    {r.product_images.find((image) => image.featured === r.featuredImageId) && (
+                      <img
+                        src={`https://sismova.tech/backsis/public/images/products/${
+                          r.product_images.find((image) => image.featured === r.featuredImageId)?.primPath
+                        }`}
+                        alt={r.prodName}
+                      />
+                    )}
                     </div>
-                    <div className="button">
-                      <Link to={`/catalogo/producto/${r.prodNumber}`}>Ver más</Link>
+                    <div className="info-prod">
+                      <div className="card-product-description">
+                        <span>{r.prodName}</span>
+                      </div>
+                      <div className="button">
+                        <Link to={`/catalogo/producto/${r.prodNumber}`}>Ver más</Link>
+                      </div>
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         }
       </section>
